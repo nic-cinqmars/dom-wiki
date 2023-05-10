24f4316ffc366f2bf70ecd644ad35ca1c0c84754
Nicolas Cinq-Mars <nic.cinqmars@gmail.com>
2023-05-09 21:07:58 -0400

    some progress

---
+++ src/components/ItemView.tsx
@@ -0,0 +1,13 @@
+import {useParams} from "react-router-dom";
+
+interface ItemViewProps
+{
+    itemName : string;
+}
+
+function ItemView()
+{
+    return <div>This is an item</div>
+}
+
+export default ItemView;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCaretRight, faCaretDown } from '@fortawesome/free-solid-svg-icons'
 import App from './App'
-import './index.css'
+
+library.add(faCaretRight, faCaretDown)
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+          <App />
+      </BrowserRouter>
   </React.StrictMode>,
 )
